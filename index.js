e0359182026b476cf5d4907702c6dded4e931aec
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-26 13:40:20 +0100

    Adds function to create an object of users

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var fs = require("fs");
+const fs = require("fs");
 
+const createObjectOfUsers = (transactionDataArray, usersObject) => {
+    transactionDataArray.forEach(transaction => {
+        usersObject[transaction[1]] = "You have no transactions"
+    })
+}
 fs.readFile("Transactions2014.csv", function(err, data) {
     if(err) throw err;
+    const transactionData = data.toString()
+    const transactionDataArray = transactionData
+                                .split("\n")
+                                .map(singleTransaction => singleTransaction.split(','))
 
-    console.log('data', data.toString());
+    const usersObject = {}
+    createObjectOfUsers(transactionDataArray, usersObject)
+    console.log(usersObject)
 });
